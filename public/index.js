1e26286863575a41c4b110c88780c0a95e696963
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-06 16:21:23 -0300

    t

--- public/index.js
+++ public/index.js
@@ -43,11 +43,3 @@ function maybeEnableButtons() {
 function gapiLoaded() {
         gapi.load('client', initializeGapiClient);
       }
-async function initializeGapiClient() {
-        await gapi.client.init({
-          apiKey: API_KEY,
-          discoveryDocs: [DISCOVERY_DOC],
-        });
-        gapiInited = true;
-        maybeEnableButtons();
-      }
