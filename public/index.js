2c07e0eabdb93735c49a86eff25c4deeed90f5b5
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-06 14:02:01 -0300

    t

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-document.querySelector("#authorize_button").addEventListener("click", handleAuthClick)
+const CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com';
+     
+
+
+document
+.querySelector("#authorize_button")
+.addEventListener("click", handleAuthClick)
+
+document
+.getElementById("signout_button")
+.addEventListener("click",handleSignoutClick)
