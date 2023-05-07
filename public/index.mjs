c98438f69755e4d482d3442c5bf74e47286214aa
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-07 19:29:33 -0300

    t

--- public/index.mjs
+++ public/index.mjs
@@ -1,5 +1,6 @@
-document.getElementById("pre").innerText="true"
-import {API_KEY, CLIENT_ID} from "../modules/credentials.mjs"
+API_KEY="AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y"
+CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com'; 
+
 let msg = document.getElementById("pos").innerText="true"
       /* exported gapiLoaded */ 
        /* exported gisLoaded */ 
