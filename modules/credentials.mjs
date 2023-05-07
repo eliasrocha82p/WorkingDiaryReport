159aed2076d1c5c19208ec87b78f2d42edff23cb
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-07 20:34:43 -0300

    t

--- modules/credentials.mjs
+++ modules/credentials.mjs
@@ -1,5 +1,5 @@
-export credentials = {
-   CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com'; 
-   API_KEY = 'AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y'; 
+export const credentials = {
+   CLIENT_ID : '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com', 
+   API_KEY : 'AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y'
 }
 export {credentials}
