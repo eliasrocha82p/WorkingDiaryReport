fe259028d325f3e2ae6c20059593a46bd49a3ed6
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-07 21:37:49 -0300

    t

--- public/index.mjs
+++ public/index.mjs
@@ -1,7 +1,7 @@
 
 import {credential} from "../modules/credentials.mjs"
-const API_KEY="AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y"
-const CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com'
+//const API_KEY="AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y"
+//const CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com'
 
       /* exported gapiLoaded */ 
        /* exported gisLoaded */ 
@@ -36,7 +36,7 @@ const CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleuser
         */ 
        async function initializeGapiClient() { 
          await gapi.client.init({ 
-           apiKey: API_KEY, 
+           apiKey: credential.API_KEY, 
            discoveryDocs: [DISCOVERY_DOC], 
          }); 
          gapiInited = true; 
@@ -48,7 +48,7 @@ const CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleuser
         */ 
        function gisLoaded() { 
          tokenClient = google.accounts.oauth2.initTokenClient({ 
-           client_id: CLIENT_ID, 
+           client_id: credential.CLIENT_ID, 
            scope: SCOPES, 
            callback: '', // defined later 
          }); 
