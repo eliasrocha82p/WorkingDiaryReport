aaa95652f0e1669c6c8cd3e2dd664e1135744991
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-08 20:59:17 -0300

    ty

--- modules/credentials.mjs
+++ modules/credentials.mjs
@@ -1,4 +1,4 @@
-export const credentials = {
+const credentials = {
    CLIENT_ID : '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com', 
    API_KEY : 'AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y'
 }
