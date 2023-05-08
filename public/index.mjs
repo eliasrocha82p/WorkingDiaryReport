46e043b3201a06b317431a11df02744a35580e99
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-07 21:24:04 -0300

    t

--- public/index.mjs
+++ public/index.mjs
@@ -1,7 +1,6 @@
 const API_KEY="AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y"
 const CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com'
 
-let msg = document.getElementById("pos").innerText="true"
       /* exported gapiLoaded */ 
        /* exported gisLoaded */ 
        /* exported handleAuthClick */ 
