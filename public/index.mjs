531931f182f361459ba91ec273829fe40a7bcc9b
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-07 21:33:37 -0300

    t

--- public/index.mjs
+++ public/index.mjs
@@ -1,3 +1,5 @@
+
+import {credential} from "../modules/credentials.mjs"
 const API_KEY="AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y"
 const CLIENT_ID = '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com'
 
