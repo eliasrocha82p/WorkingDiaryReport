05dd43eedf5b89b09d1915836bf8006aa5240e75
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-08 21:12:41 -0300

    tty

--- modules/credentials.mjs
+++ modules/credentials.mjs
@@ -1,5 +1,4 @@
-const credentials = {
+export default credentials = {
    CLIENT_ID : '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com', 
    API_KEY : 'AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y'
 }
-export {credentials}
