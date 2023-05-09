52afc5d7fa610777336f168d3a1f96b8093902ee
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-09 10:03:22 -0300

    t

--- modules/credentials.mjs
+++ modules/credentials.mjs
@@ -1,4 +1,8 @@
-export default credentials = {
+export const credentials = {
    CLIENT_ID : '782340290651-dvh3coksj8oplephm4h6u4d8pdi7i4jv.apps.googleusercontent.com', 
    API_KEY : 'AIzaSyA3KY-zyck80RUM-8POgcTYiKEtGTmLE6Y'
+};
+export function getKey(){
+   alert(credentials.API_KEY)
+   return credentials.API_KEY
 }
--- public/App.mjs
+++ public/App.mjs
@@ -1,4 +1,5 @@
-import {credentials} from "credentials.mjs"
+import {credentials,getKey} from "credentials.mjs"
+alert(getKey())
 const p=document.createElement("h1")
 p.innerHTML="Ola elias2"
 document.body.appendChild(p)
