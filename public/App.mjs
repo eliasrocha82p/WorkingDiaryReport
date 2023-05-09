d7df2508a204e23e145455434aa20b33d7ccba89
eliasrocha82p@gmail.com <eliasrocha82p@gmail.com>
2023-05-09 05:15:55 -0300

    y

--- public/App.mjs
+++ public/App.mjs
@@ -1,4 +1,4 @@
-import credentials from "./credentials.mjs"
+import {credentials} from "credentials.mjs"
 const p=document.createElement("h1")
 p.innerHTML="Ola elias2"
 document.body.appendChild(p)
